4380b3a1631ddcc19bcee6f759de9200ed555b84
saksham-accio <125554544+saksham-accio@users.noreply.github.com>
2023-06-24 09:31:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-//your JS code here. If required.
+//your code here
+const btn = document.getElementById("btn");
+const age = document.getElementById("age");
+const name = document.getElementById("name");
+
+btn.addEventListener("click", sol);
+
+function sol() {
+  const ageVal = age.value;
+  const nameVal = name.value;
+
+  if (!(ageVal && nameVal)) {
+    alert("Please enter valid details");
+    return;
+  }
+
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (ageVal > 18) {
+        alert(`Welcome, ${nameVal}. You can vote.`);
+      } else {
+        alert(`Oh sorry ${nameVal}. You aren't old enough.`);
+      }
+    }, 4000);
+  });
+}
